da1aa29074d2cd7d2d0f727ff75b882792875a1a
AndriyBilous <andriy.bilous.92@gmail.com>
2022-11-22 14:03:23 +0200

    start of commits

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://cdn.pixabay.com/photo/2022/10/07/12/02/sunset-7504891_960_720.jpg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <main className="content">
+      <img src="https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg"></img>
+      <img src="https://cdn.pixabay.com/photo/2017/11/13/07/14/cats-eyes-2944820_960_720.jpg"></img>
+      Main content
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
