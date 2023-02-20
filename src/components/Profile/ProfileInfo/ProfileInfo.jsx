523bbcf1cba7eb2848c50f691cc9ed0dbc2204ec
Andriy Bilous <andriy.bilous.92@gmail.com>
2023-02-20 15:46:43 +0200

    Changed profileStatus component from class component to functional component with hooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Preloader from "../../common/Preloader";
 import styles from './ProfileInfo.module.css'
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -19,7 +20,7 @@ const ProfileInfo = (props) => {
           <img src={props.profile.photos.large} alt="" />
         </div>
         <div>{props.profile.aboutMe}</div>
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
       </div>
     );
 }
