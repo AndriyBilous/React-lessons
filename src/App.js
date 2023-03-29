c6654faba1aa5da9ee2589b7b2f7b405a338ce26
Andriy Bilous <andriy.bilous.92@gmail.com>
2023-03-29 16:05:10 +0300

    Added suspender for dialogs and profile pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { lazy } from "react";
 import { connect } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { compose } from "redux";
 import "./App.css";
 import Preloader from "./components/common/Preloader";
-import DialogsContainer from "./components/Dialogs/DialogsContainer.jsx";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer.jsx";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import Navigation from "./components/Navigation/Navigation.jsx";
-import ProfileContainer, { withRouter } from "./components/Profile/ProfileContainer";
+import { withRouter } from "./components/Profile/ProfileContainer";
 import UsersContainer from "./components/Users/UsersContainer.jsx";
 import { initializeApp } from "./redux/appReduser";
 
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/reduxStore'
 import { Provider } from 'react-redux';
+import { withSuspense } from "./hoc/withSuspense";
+
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer.jsx'));
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+const WithSuspenseDialogsContainer = withSuspense(DialogsContainer); 
+const WithSuspenseProfileContainer = withSuspense(ProfileContainer);
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,9 +37,9 @@ class App extends React.Component {
           <Navigation />
           <div className="app-wrapper__content">
             <Routes >
-              <Route path="/profile/:userId" element={<ProfileContainer/>}/>
-              <Route path="/profile/*" element={<ProfileContainer/>}/>
-              <Route path="dialogs/*" element={<DialogsContainer/>}/>
+              <Route path="/profile/:userId" element={<WithSuspenseProfileContainer/>}/>
+              <Route path="/profile/*" element={<WithSuspenseProfileContainer/>}/>
+              <Route path="dialogs/*" element={ <WithSuspenseDialogsContainer/>}/>
               <Route path="users/*" element={<UsersContainer/>}/>
               <Route path="login/*" element={<Login/>}/>
             </Routes>
